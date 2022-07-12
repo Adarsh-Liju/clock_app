5ffc80e35417a8fc3b65f27cdb31dbb646abbb80
Adarsh Liju Abraham <adarsh.liju.abraham@gmail.com>
2022-07-13 00:04:39 +0530

    Added date and time
    Signed-off-by: Adarsh Liju Abraham <adarsh.liju.abraham@gmail.com>

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,5 +1,6 @@
 import 'package:clock_app/clock_view.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -9,11 +10,39 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
+    var now = DateTime.now();
+    var formattedTime = DateFormat('HH:mm').format(now);
+    var formattedDate = DateFormat('EEE, d MMM').format(now);
+
     return Scaffold(
-      body: Container(
-        alignment: Alignment.center,
-        color: Color(0XFF2D2F41),
-        child: ClockView(),
+      backgroundColor: Color(0XFF2D2F41),
+      body: Center(
+        child: Row(
+          children: <Widget>[
+            Container(
+              padding: EdgeInsets.all(32),
+              alignment: Alignment.center,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Text(
+                    'Clock',
+                    style: TextStyle(color: Colors.white, fontSize: 24),
+                  ),
+                  Text(
+                    formattedTime,
+                    style: TextStyle(color: Colors.white, fontSize: 64),
+                  ),
+                  Text(
+                    formattedDate,
+                    style: TextStyle(color: Colors.white, fontSize: 20),
+                  ),
+                  ClockView(),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
